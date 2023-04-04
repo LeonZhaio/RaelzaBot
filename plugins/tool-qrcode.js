99d07947ad1b09a6a8b5410de11755ed56d9c0d7
dnm <74846398+adamdani169@users.noreply.github.com>
2023-04-04 19:16:16 +0700

    Update tool-qrcode.js

--- plugins/tool-qrcode.js
+++ plugins/tool-qrcode.js
@@ -1,7 +1,7 @@
 import { toDataURL } from 'qrcode'
 
 let handler = async (m, { conn, text }) => {
-    if (!text) return conn.reply(m.chat, 'Use example: \n.qrcode Shirokami Ryzen', m)
+    if (!text) return conn.reply(m.chat, 'Use example: \n.qrcode dnm', m)
 
 conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
 }
