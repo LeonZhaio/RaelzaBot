d411e0ce7cdb0efdbcd5af7b317f7376f2c2b01d
LeonZhaio <102395522+LeonZhaio@users.noreply.github.com>
2023-04-16 10:59:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -82,7 +82,7 @@ global.lynk = 'https://lynk.id/'
 
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ MEDIA SOSSIAL ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.sig = 'https://www.instagram.com/adam_daniam'
+global.sig = 'https://fantox-cosplay-api.onrender.com'
 global.sgh = 'https://github.com/LeonZhaio/' //github
 global.sgc = 'https://chat.whatsapp.com/CnEQWo8eQEKI1AMUSfO9nm' //group whatsapp
 global.sdc = '-' //discord
